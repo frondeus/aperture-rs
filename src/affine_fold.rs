3e48e44a6be26290a359273ab13fa55b33ff70a2
Wojciech Polak <wp@impero.com>
2022-07-26 23:10:07 +0200

    AF AT

--- src/affine_fold.rs
+++ src/affine_fold.rs
@@ -51,6 +51,23 @@ where
     }
 }
 
+impl<L1, L2, S> Fold<AsFold, S> for And<L1, L2, (AsAffineFold, AsTraversal), (S, L1::T)>
+where
+    L1: AffineFold<AsAffineFold, S>,
+    L2: Fold<AsTraversal, L1::T>,
+    L2::D: Iterator,
+{
+    type D = std::iter::Flatten<std::option::IntoIter<L2::D>>;
+
+    fn fold(&self, source: S) -> Self::D {
+        self.0
+            .preview(source)
+            .map(|t| self.1.fold(t))
+            .into_iter()
+            .flatten()
+    }
+}
+
 impl<L1, L2, S> AffineFold<AsAffineFold, S>
     for And<L1, L2, (AsAffineFold, AsAffineTraversal), (S, L1::T)>
 where
@@ -94,14 +111,6 @@ mod tests {
         assert_eq!(grand_grandma, None);
     }
 
-    // #[test]
-    // fn af_and_setter() {
-    //     let lens = PersonMotherAF.then(PersonNameSetter);
-
-    //     let mom = lens.set(Person::wojtek(), |name| name.to_uppercase());
-    //     assert_eq!(mom.unwrap().name, "MIROSLAWA");
-    // }
-
     #[test]
     fn af_and_fold() {
         let lens = PersonParentsAF.then(ListOf);
@@ -119,19 +128,12 @@ mod tests {
         assert_eq!(grandma.unwrap().name, "Lidia");
     }
 
-    // #[test]
-    // fn af_and_traversal() {
-    //     let lens = PersonParentsAF.then(Every);
+    #[test]
+    fn af_and_traversal() {
+        let lens = PersonParentsAF.then(Every);
 
-    //     let mut parents = lens.traverse(Person::wojtek(), |x| x);
-    //     assert_eq!(parents.next().unwrap().name, "Miroslawa");
-    //     assert_eq!(parents.next().unwrap().name, "Zenon");
-
-    //     let wojtek = lens.set(Person::wojtek(), |mut x| {
-    //         x.name = x.name.to_uppercase();
-    //         x
-    //     });
-    //     assert_eq!(wojtek.parents[0].name, "MIROSLAWA");
-    //     assert_eq!(wojtek.parents[1].name, "ZENON");
-    // }
+        let mut parents = lens.fold(Person::wojtek());
+        assert_eq!(parents.next().unwrap().name, "Miroslawa");
+        assert_eq!(parents.next().unwrap().name, "Zenon");
+    }
 }
